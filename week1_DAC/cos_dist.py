d9ed89df11e167059e46c6635322aa2fe38fe096
Seyong Kim <seyong@yonsei.ac.kr>
2022-04-23 21:31:35 +0900

    fix

--- week1_DAC/cos_dist.py
+++ week1_DAC/cos_dist.py
@@ -5,4 +5,5 @@ def get_cos_dist_matrix(features: torch.Tensor):
     norm_feat = torch.exp(features - torch.max(features))
     l2norm = torch.norm(norm_feat, dim=1, keepdim=True)
     unit_vec = norm_feat / l2norm
+
     return torch.mm(unit_vec, unit_vec.transpose(0, 1))
