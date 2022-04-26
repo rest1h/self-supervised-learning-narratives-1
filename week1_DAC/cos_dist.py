5cc29a4ccc90a2c3abd8ac6e5cd691f0e8b16f5d
Seyong Kim <seyong@yonsei.ac.kr>
2022-04-26 14:44:48 +0900

    use the upper triangular matrix

--- week1_DAC/cos_dist.py
+++ week1_DAC/cos_dist.py
@@ -5,5 +5,6 @@ def get_cos_dist_matrix(features: torch.Tensor):
     norm_feat = torch.exp(features - torch.max(features))
     l2norm = torch.norm(norm_feat, dim=1, keepdim=True)
     unit_vec = norm_feat / l2norm
+    cos_dist = torch.mm(unit_vec, unit_vec.transpose(0, 1))
 
-    return torch.mm(unit_vec, unit_vec.transpose(0, 1))
+    return torch.triu(cos_dist)
